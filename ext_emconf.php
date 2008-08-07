ab39cf32a4d4b6261ef6a47be624c52fb543ded0
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2008-08-07 14:12:57 +0000

    Version 0.2.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'T3sports bet system',
-	'description' => 'Bet-system for T3sports. FE-Users can bet on matches in T3sports.',
+	'description' => 'Bet-system for T3sports. FE-Users can bet on matches in T3sports. Tippspiel auf Basis von T3sports.',
 	'category' => 'plugin',
 	'author' => 'Rene Nitzsche',
 	'author_email' => 'rene@system25.de',
@@ -27,13 +27,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'System 25',
-	'version' => '0.2.6',
+	'version' => '0.2.7',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
 			'div' => '0.0.11-0.0.0',
 			'lib' => '0.1.0-0.0.0',
-			'rn_base' => '0.2.6-0.0.0',
+			'rn_base' => '0.2.7-0.0.0',
 			'cfc_league' => '0.4.5-0.0.0',
 			'cfc_league_fe' => '0.4.7-0.0.0',
 			't3users' => '0.1.1-0.0.0',
