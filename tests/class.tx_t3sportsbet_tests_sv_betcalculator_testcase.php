ac4795286b0ef60d0d7ddb7f877cfff764f51fe1
digedag <digedag@b9a124fc-2ab3-4098-ad7a-080e7ed9e93c>
2008-12-03 19:49:03 +0000

    Point calculation for cup matches fixed.

--- tests/class.tx_t3sportsbet_tests_sv_betcalculator_testcase.php
+++ tests/class.tx_t3sportsbet_tests_sv_betcalculator_testcase.php
@@ -67,6 +67,31 @@ class tx_t3sportsbet_tests_sv_betcalculator_testcase extends tx_phpunit_testcase
 		$result = $calculator->calculatePoints($betgame, $bet);
 		$this->assertEquals(5, $result, 'Match 3:0 Bet 3:0 Points: ' . $result);
 	}
+	function test_betCalculationCup() {
+		$betgame = $this->getBetgame(5,0,1,1);
+		$betgame2 = $this->getBetgame(5,0,1,0);
+		$calculator = tx_div::makeInstance('tx_t3sportsbet_services_betcalculator');
+		$matches = $this->getMatches();
+		
+		$bet = $this->getBet(3,1);
+		$bet->record['t3match'] = $matches['match_3_1_et']->uid;
+		$result = $calculator->calculatePoints($betgame, $bet);
+		$this->assertEquals(0, $result, 'Match 3:1 et (1:1) Bet 3:1 Points: ' . $result);
+		$result = $calculator->calculatePoints($betgame2, $bet);
+		$this->assertEquals(5, $result, 'Match 3:1 et (1:1) Bet 3:1 Points: ' . $result);
+		
+		$bet = $this->getBet(2,2);
+		$bet->record['t3match'] = $matches['match_3_1_et']->uid;
+		$result = $calculator->calculatePoints($betgame, $bet);
+		$this->assertEquals(1, $result, 'Match 3:1 et (1:1) Bet 2:2 Points: ' . $result);
+		$result = $calculator->calculatePoints($betgame2, $bet);
+		$this->assertEquals(0, $result, 'Match 3:1 et (1:1) Bet 3:1 Points: ' . $result);
+		
+		$bet = $this->getBet(1,1);
+		$bet->record['t3match'] = $matches['match_3_1_et']->uid;
+		$result = $calculator->calculatePoints($betgame, $bet);
+		$this->assertEquals(5, $result, 'Match 3:1 et (1:1) Bet 1:1 Points: ' . $result);
+	}
 	function test_betCalculationWoDiff() {
 		// Ohne Tordiff testen
 		$betgame = $this->getBetgame(5,0,1);
@@ -88,13 +113,15 @@ class tx_t3sportsbet_tests_sv_betcalculator_testcase extends tx_phpunit_testcase
 		$this->assertEquals(5, $result, 'Match 3:0 Bet 3:0 Points: ' . $result);
 	}
 
-	private function getBetgame($p1, $p2, $p3) {
+	private function getBetgame($p1, $p2, $p3, $drawIfET=0, $drawIfPenalty=0) {
 		$clazzname = tx_div::makeInstanceClassname('tx_t3sportsbet_models_betgame');
 		$record = array();
 		$record['uid'] = 1;
 		$record['points_accurate'] = $p1;
 		$record['points_goalsdiff'] = $p2;
 		$record['points_tendency'] = $p3;
+		$record['draw_if_extratime'] = $drawIfET;
+		$record['draw_if_penalty'] = $drawIfPenalty;
 		return new $clazzname($record);
 	}
 	private function getBet($home, $guest) {
