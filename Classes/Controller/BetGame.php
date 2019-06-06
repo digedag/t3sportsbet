d1bea353deda2f7a45a5c4d9a8ab9a03597ef0b3
René Nitzsche <nitzsche@dmk-ebusiness.de>
2019-06-06 20:59:18 +0200

    Classes folder in composer.json

---
+++ Classes/Controller/BetGame.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Sys25\T3sportsbet\Controller;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2008-2019 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Die Klasse ist die Einstiegsklasse für das Modul "Tippspiel"
+ */
+class BetGame extends \tx_rnbase_mod_BaseModFunc
+{
+
+    private $doc, $MCONF;
+
+    /**
+     * Method getFuncId
+     *
+     * @return string
+     */
+    protected function getFuncId()
+    {
+        return 'funcbetgame';
+    }
+
+    /**
+     *
+     * @param string $template
+     * @param \tx_rnbase_configurations $configurations
+     * @param \tx_rnbase_util_FormatUtil $formatter
+     * @param \tx_rnbase_util_FormTool $formTool
+     * @return string
+     */
+    protected function getContent($template, &$configurations, &$formatter, $formTool)
+    {
+        $modContent = 'Hello MOD!';
+
+        return $modContent;
+    }
+}
+
--- ext_tables.php
+++ ext_tables.php
@@ -2,17 +2,6 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 
-if(!tx_rnbase_util_TYPO3::isTYPO62OrHigher()) {
-    // TCA registration for 4.5
-    $TCA['tx_t3sportsbet_betgames'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/locallang_db.xml:tx_t3sportsbet_betgames.php';
-    $TCA['tx_t3sportsbet_betsets'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/locallang_db.xml:tx_t3sportsbet_betsets.php';
-    $TCA['tx_t3sportsbet_betsetresults'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/locallang_db.xml:tx_t3sportsbet_betsetresults.php';
-    $TCA['tx_t3sportsbet_bets'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/locallang_db.xml:tx_t3sportsbet_bets.php';
-    $TCA['tx_t3sportsbet_teamquestions'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/locallang_db.xml:tx_t3sportsbet_teamquestions.php';
-    $TCA['tx_t3sportsbet_teambets'] = require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/locallang_db.xml:tx_t3sportsbet_teambets.php';
-}
-
-
 ////////////////////////////////
 // Plugin anmelden
 ////////////////////////////////
@@ -34,19 +23,15 @@ tx_rnbase_util_Extensions::addStaticFile($_EXTKEY,'static/css/', 'T3sports Bet-S
 if (TYPO3_MODE=='BE')	{
 	tx_rnbase::load('tx_rnbase_util_Wizicon');
 	tx_rnbase_util_Wizicon::addWizicon('tx_t3sportsbet_util_Wizicon', tx_rnbase_util_Extensions::extPath($_EXTKEY).'util/class.tx_t3sportsbet_util_Wizicon.php');
-}
 
-////////////////////////////////
-// Submodul anmelden
-////////////////////////////////
-if (TYPO3_MODE=="BE")	{
-    $modName = 'web_txcfcleagueM1';
-    if (tx_rnbase_util_TYPO3::isTYPO60OrHigher()) {
-        $modName = 'web_CfcLeagueM1';
-    }
-    require_once(tx_rnbase_util_Extensions::extPath($_EXTKEY) .'util/class.tx_t3sportsbet_util_ItemFunctions.php');
-	tx_rnbase_util_Extensions::insertModuleFunction($modName, 'tx_t3sportsbet_mod1_index',
-	    tx_rnbase_util_Extensions::extPath($_EXTKEY).'mod1/class.tx_t3sportsbet_mod1_index.php',
+    ////////////////////////////////
+    // Submodul anmelden
+    ////////////////////////////////
+	$modName = 'web_CfcLeagueM1';
+
+//	require_once(tx_rnbase_util_Extensions::extPath($_EXTKEY) .'util/class.tx_t3sportsbet_util_ItemFunctions.php');
+	tx_rnbase_util_Extensions::insertModuleFunction($modName, \Sys25\T3sportsbet\Controller\BetGame::class,
+	    tx_rnbase_util_Extensions::extPath($_EXTKEY) . 'Classes/Controller/BetGame.php',
 		'LLL:EXT:t3sportsbet/locallang_db.xml:tx_t3sportsbet_module.name'
 	);
 }
